d6a47d004407f3cb94ecbc04e29a3671f9aedc3e
andy <ck@techecho.io>
2024-03-06 12:38:29 +0400

    init

---
+++ d12init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update && sudo apt -y upgrade
+
+sudo apt install -y curl gnupg2 ca-certificates lsb-release apt-transport-https vim screen wget iproute2 net-tools pkg-config libssl-dev build-essential tcpdump
+
+sudo bash -c 'cat << EOF >> /etc/sysctl.conf
+net.ipv4.ip_forward=1
+net.ipv6.conf.all.forwarding=1
+EOF'
+sudo sysctl -p
