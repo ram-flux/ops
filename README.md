ca388b317358ff097dc294aea9f95f3fd46f7244
andy <ck@techecho.io>
2024-03-17 12:49:02 +0400

    add i sudo

---
+++ README.md
@@ -0,0 +1,5 @@
+# d12 sys init
+ - wget --no-check-certificate -qO- https://raw.githubusercontent.com/ram-flux/ops/main/d12init.sh | bash
+ 
+ - sudo wget --no-check-certificate -qO- https://raw.githubusercontent.com/ram-flux/ops/main/d12init.sh | bash
+ 
--- d12init.sh
+++ d12init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update && sudo apt -y upgrade
 
-sudo apt install -y curl gnupg2 ca-certificates lsb-release apt-transport-https vim screen wget iproute2 net-tools pkg-config libssl-dev build-essential tcpdump
+sudo apt install -y curl gnupg2 ca-certificates lsb-release apt-transport-https sudo vim screen wget iproute2 net-tools pkg-config libssl-dev build-essential tcpdump
 
 sudo bash -c 'cat << EOF >> /etc/sysctl.conf
 net.ipv4.ip_forward=1
